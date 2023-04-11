8d82f8ad44031901f70df512fd3313fcfdd2a4b0
tinoh9 <bachhuynh959@gmail.com>
2023-04-11 14:22:35 +0700

    updated structs and methods

--- src/bankcore/bank.go
+++ src/bankcore/bank.go
@@ -1,5 +1,49 @@
 package bank
 
-func Hello() string {
-	return "I am here!"
+import (
+	"errors"
+	"fmt"
+)
+
+// Customer struct
+type Customer struct {
+	Name 	string
+	Address string
+	Phone 	string
+}
+
+// Account struct
+type Account struct {
+	Customer
+	Number 	int32
+	Balance float64
+}
+
+// Deposit method
+func (a *Account) Deposit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("You cannot deposit zero or negative amount!")
+	}
+
+	a.Balance += amount
+	return nil
+}
+
+// Withdraw method
+func (a *Account) Withdraw(amount float64) error {
+	if amount <= 0 {
+		return errors.New("You cannot withdraw zero or negative amount!")
+	}
+
+	if amount > a.Balance {
+		return errors.New("You cannot withdraw the amount which is larger than the account balance!")
+	}
+
+	a.Balance -= amount
+	return nil
+}
+
+// Statement method
+func (a *Account) Statement() string {
+	return fmt.Sprintf("%v - %v - %v", a.Number, a.Name, a.Balance)
 }
--- src/bankcore/bank_test.go
+++ src/bankcore/bank_test.go
@@ -13,6 +13,73 @@ func TestAccount(t *testing.T) {
 		Balance: 0,
 	}
 	if account.Name == "" {
-		t.Error ("Cannot create anAccount object")
+		t.Error("Cannot create an Account object")
+	}
+}
+
+func TestDeposit(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name: "Adam",
+			Address: "Westminster, California",
+			Phone: "(123) 456 789",
+		},
+		Number: 1001,
+		Balance: 0,
+	}
+	account.Deposit(10)
+
+	if account.Balance != 10 {
+		t.Error("Balance is not being updated after the deposit!")
+	}
+}
+
+func TestInvalidDeposit(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name: "Adam",
+			Address: "Westminster, California",
+			Phone: "(123) 456 789",
+		},
+		Number: 1001,
+		Balance: 0,
+	}
+	if err := account.Deposit(-10); err == nil {
+		t.Error("Only positive number is allowed to deposit!")
+	}
+}
+
+func TestWithdraw(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name: "Adam",
+			Address: "Westminster, California",
+			Phone: "(123) 456 789",
+		},
+		Number: 1001,
+		Balance: 0,
+	}
+	account.Deposit(10)
+	account.Withdraw(10)
+
+	if account.Balance != 0 {
+		t.Error("Balance is not being updated after withdraw!")
+	}
+}
+
+func TestStatement(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name: "Adam",
+			Address: "Westminster, California",
+			Phone: "(123) 456 789",
+		},
+		Number: 1001,
+		Balance: 0,
+	}
+	account.Deposit(500)
+	statement := account.Statement()
+	if statement != "1001 - Adam - 500" {
+		t.Error("Account statement does not have the proper format!")
 	}
 }
