568e52b9206ca9278f732c56df33c2894d62c2ef
tinoh9 <bachhuynh959@gmail.com>
2023-04-13 10:09:10 +0700

    updated deposit & withdraw method

--- src/bankapi/main.go
+++ src/bankapi/main.go
@@ -59,6 +59,34 @@ func deposit(w http.ResponseWriter, req *http.Request) {
     }
 }
 
+func withdraw(w http.ResponseWriter, req *http.Request) {
+    numberqs := req.URL.Query().Get("number")
+    amountqs := req.URL.Query().Get("amount")
+
+    if numberqs == "" {
+        fmt.Fprintf(w, "Account number is missing")
+        return
+    }
+
+    if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid account number")
+    } else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+        fmt.Fprintf(w, "Invalid amount number")
+    } else {
+        account, ok := accounts[number]
+        if !ok {
+            fmt.Fprintf(w, "Account with number %v can't be found", number)
+        } else {
+            err := account.Withdraw(amount)
+            if err != nil {
+                fmt.Fprintf(w, "%v", err)
+            } else {
+                fmt.Fprintf(w, account.Statement())
+            }
+        }
+    }
+}
+
 func main() {
     accounts[101] = &bank.Account{
 		Customer: bank.Customer{
@@ -70,5 +98,7 @@ func main() {
 	}
 
 	http.HandleFunc("/statement", statement)
+    http.HandleFunc("/deposit", deposit)
+    http.HandleFunc("/withdraw", withdraw)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
